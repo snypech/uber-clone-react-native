3f21105497bfe32df14ad54846498dcda9b7aba7
snypech <sneyder.yepes@gmail.com>
2021-11-10 16:04:09 -0500

    add screen component with rnfes

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Text>My first react navtive app</Text>
-        <StatusBar style="auto" />
+        <HomeScreen />
       </View>
     </Provider>
   );
