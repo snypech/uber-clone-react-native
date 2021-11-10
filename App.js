9fe09d40aae2f25552f52c8907fea4c048fe9ee0
snypech <sneyder.yepes@gmail.com>
2021-11-10 15:46:31 -0500

    create navSlice and store from redux documentation

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My first react navtive app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>My first react navtive app</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
