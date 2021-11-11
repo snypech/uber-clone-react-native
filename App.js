55645704141f7c09dda3deb2fd3862c9fd3b55d3
snypech <sneyder.yepes@gmail.com>
2021-11-10 22:51:51 -0500

    Icon from react-native-elements, like fontawesome, and some styling

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View>
+      <SafeAreaProvider>
         <HomeScreen />
-      </View>
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import tw from 'tailwind-react-native-classnames'
+import React from 'react';
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Icon } from 'react-native-elements';
+import tw from 'tailwind-react-native-classnames';
 
 const data=[
   {
@@ -31,6 +32,12 @@ const NavOptions = () => {
           source={{uri:item.image}}
           />
           <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+          <Icon
+          style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+          name="arrowright"
+          color="white" 
+          type="antdesign"
+          />
         </View>
       </TouchableOpacity>
     )}
