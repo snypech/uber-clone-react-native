a61ec38e8898824357ff5ea23858a2e21cb02b60
snypech <sneyder.yepes@gmail.com>
2021-11-11 16:35:27 -0500

    install @react-navigation/stack

--- App.js
+++ App.js
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <HomeScreen />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
