50be7848ba2bf015e1bb2748f6341aa2956b24b2
snypech <sneyder.yepes@gmail.com>
2021-11-15 12:52:41 -0500

    install Map directions, implement in map component, add lineDashPattern to fix error

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { StyleSheet} from 'react-native'
 import MapView, {Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 import { useSelector } from 'react-redux';
 import tw from 'tailwind-react-native-classnames';
-import { selectOrigin } from '../slices/navSlice';
+import { selectDestination, selectOrigin } from '../slices/navSlice';
+import {GOOGLE_MAPS_APIKEY} from "@env";
 
 const Map = () => {
   const origin = useSelector(selectOrigin);//redux selector
+  const destination = useSelector(selectDestination)//redux selector
   return (
     <MapView
     style={tw`flex-1`}
@@ -18,6 +21,18 @@ const Map = () => {
       longitudeDelta: 0.005,
     }}
     >
+      {console.log(origin,destination)}
+      {origin && destination && (
+        <MapViewDirections
+        origin={origin.description}
+        destination={destination.description}
+        apikey={GOOGLE_MAPS_APIKEY}
+        strokeWidth={3}
+        strokeColor="black"
+        lineDashPattern={[50]}
+
+        />
+      )}
     {origin?.location && (
       <Marker
         coordinate={{
