4087ac5969cd13f8d72644efdf2c0d85c155b569
snypech <sneyder.yepes@gmail.com>
2021-11-15 13:12:40 -0500

    add marker to destination and zoom out-in to the two markers, by using useref to the map, useeffecto to ori-dest and the fucntion fittosuppliedmarkers

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { StyleSheet} from 'react-native'
 import MapView, {Marker} from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
@@ -10,8 +10,17 @@ import {GOOGLE_MAPS_APIKEY} from "@env";
 const Map = () => {
   const origin = useSelector(selectOrigin);//redux selector
   const destination = useSelector(selectDestination)//redux selector
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if(!origin || !destination) return;
+    mapRef.current.fitToSuppliedMarkers(['origin','destination'],{
+      edgePadding:{top:50,right:50,bottom:50,left:50}
+    })
+  }, [origin,destination])
   return (
     <MapView
+    ref={mapRef}
     style={tw`flex-1`}
     mapType="mutedStandard"
     initialRegion={{
@@ -42,6 +51,17 @@ const Map = () => {
         title='Origin'
         description={origin.description}
         identifier="origin"
+      />
+    )}  
+     {destination?.location && (
+      <Marker
+        coordinate={{
+          latitude: destination.location.lat,
+          longitude: destination.location.lng,
+        }}
+        title='Destination'
+        description={destination.description}
+        identifier="destination"
       />
     )}  
     </MapView>
